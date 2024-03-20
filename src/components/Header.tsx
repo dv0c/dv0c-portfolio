538de874f3b7916bb2c850c78f23242d743d57f1
dv0c <tasosmeidanis12@gmail.com>
2024-03-20 10:42:31 +0200

    new file:   components.json 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/(routes)/(home)/page.tsx 	deleted:    src/app/globals.css 	modified:   src/app/layout.tsx 	deleted:    src/app/page.tsx 	new file:   src/components/Header.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/lib/utils.ts 	new file:   src/styles/globals.css 	modified:   tailwind.config.ts

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import { Menu } from "lucide-react"
+import { Button } from "./ui/button"
+
+const Header = () => {
+    return <header className="py-10">
+        <div className="flex items-center justify-between">
+            <section className="flex items-center gap-10">
+                <Button className="p-2" variant={'ghost'}>
+                    <Menu className="block md:hidden" />
+                </Button>
+                <h1 className="text-xl font-semibold">Tasos</h1>
+                <ul className="hidden md:flex items-center">
+                    <li>
+                        <Button variant={'link'}>
+                            Services
+                        </Button>
+                    </li>
+                    <li>
+                        <Button variant={'link'}>
+                            About me
+                        </Button>
+                    </li>
+                    <li>
+                        <Button variant={'link'}>
+                            Contact us
+                        </Button>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <Button variant={'outline'}>
+                    See my work
+                </Button>
+            </section>
+        </div>
+    </header>
+}
+
+export default Header

4079c7d06e7582ea12eafc53795e1b480b41b4e0
dv0c <tasosmeidanis12@gmail.com>
2024-03-21 15:19:22 +0200

    modified:   src/components/Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,42 @@
-import { font } from '@/lib/font';
-import Image from 'next/image';
-import Link from 'next/link';
-import { FC } from 'react'
+import { font } from "@/lib/font";
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
 
 interface CardProps {
-    title: string;
-    description: string;
-    image: any;
-    href: string;
+  title: string;
+  description: string;
+  image: any;
+  href: string;
 }
 
 export const Card: FC<CardProps> = ({ description, href, image, title }) => {
-    return <Link href={href} type='button' className='border bg-background rounded-2xl p-5 group'>
-        <div className='rounded-xl relative overflow-hidden w-full h-[300px]'>
-            <Image alt={title} src={image} fill placeholder='blur' className='object-cover' />
-        </div>
-        <div className='mt-5 space-y-2'>
-            <h1 className={'font-semibold text-2xl text-stone-800 group-hover:underline ' + font.className}>{title}</h1>
-            <p className='text-muted-foreground text-sm'>{description}</p>
-        </div>
+  return (
+    <Link
+      href={href}
+      type="button"
+      className="border bg-background rounded-2xl p-5 group"
+    >
+      <div className="rounded-xl relative overflow-hidden w-full h-[300px]">
+        <Image
+          alt={title}
+          src={image}
+          fill
+          placeholder="blur"
+          className="object-cover"
+        />
+      </div>
+      <div className="mt-5 space-y-2">
+        <h1
+          className={
+            "font-semibold text-xl md:text-2xl text-stone-800 group-hover:underline " +
+            font.className
+          }
+        >
+          {title}
+        </h1>
+        <p className="text-muted-foreground text-sm">{description}</p>
+      </div>
     </Link>
-}
+  );
+};
